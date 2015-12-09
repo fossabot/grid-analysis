b821c975a896ced0cbddd02b1d313d7b5ac0cc79
willfurnell <willfurnell@users.noreply.github.com>
2015-12-09 18:22:22 +0000

    Testing out some JSON stuff, converted to Python 2.7

--- analyse.py
+++ analyse.py
@@ -8,13 +8,21 @@ x,y,C format.
 Requires lucid-utils for actually analysing files.
 
 """
+# Makes it easy to convert between Python 2 and Python 3
+from __future__ import print_function
 
-
+# Required for analysis of files
 from lucid_utils import blobbing, xycreader
 
 from lucid_utils.classification.old_algorithm import classify
 
+# To get arguments from user
 import argparse
+
+# For output use
+import json
+
+import time
 
 parser = argparse.ArgumentParser(description='Analyse some files.')
 parser.add_argument('folder', metavar='F', type=str, nargs='+',
@@ -33,7 +41,9 @@ def analyse_frame(frame):
     return counts
 
 
-def analyse_folder(folder):
+def analyse_folder(folder, final_output):
+
+    output = []
 
     # This will NOT work currently - just for reference
     for file in folder:
@@ -44,8 +54,30 @@ def analyse_folder(folder):
 
         counts = analyse_frame(frame)
 
-        f = open("counts", 'w')
+        final_output["frames"] = {}
+        final_output["frames"][filename] = {}
+        final_output["frames"]["filename"]["counts"] = counts
 
-        f.write(filename + "|" + str(counts) + "\n")
+        #f = open("counts.json", 'w')
 
-        f.close()
+        #f.write(json.dumps()"\n")
+
+        #f.close()
+
+
+if __name__ == "__main__":
+    print("Main program logic")
+
+    filename = "cas000"
+
+    counts = {'alpha': 0, 'beta': 0, 'gamma': 0, 'proton': 0, 'muon': 0, 'other': 0}
+
+    final_output = {}
+
+    final_output["metadata"] = {}
+    final_output["metadata"]["generator"] = 'grid-analysis'
+    final_output["metadata"]["gentime"] = time.time()
+
+
+    print(final_output)
+
